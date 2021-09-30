37a2307ec41f5f5feda7420f3d2bacf5845495fb
LeticiaAquino1 <87331393+LeticiaAquino1@users.noreply.github.com>
2021-09-29 22:44:42 -0300

    Criação classe PacienteView e correção classe paciente

--- src/poo/trabalho/modal/Paciente.java
+++ src/poo/trabalho/modal/Paciente.java
@@ -4,23 +4,15 @@ import java.util.Date;
 
 public class Paciente extends Pessoa {
 	
-	private Pessoa pessoa;
 	private FichaMedica fichaMedica;
 
-	public Paciente(String nome, String endereco, char sexo, Date dataNascimento, Pessoa pessoa, FichaMedica fichaMedica) {
+	public Paciente(String nome, String endereco, char sexo, Date dataNascimento, FichaMedica fichaMedica) {
 		super(nome, endereco, sexo, dataNascimento);
 		
-		this.pessoa = pessoa;
+
 		this.fichaMedica = fichaMedica;
 	}
 
-	public Pessoa getPessoa() {
-		return pessoa;
-	}
-
-	public void setPessoa(Pessoa pessoa) {
-		this.pessoa = pessoa;
-	}
 
 	public FichaMedica getFichaMedica() {
 		return fichaMedica;
--- src/poo/trabalho/view/PacienteView.java
+++ src/poo/trabalho/view/PacienteView.java
@@ -1,5 +1,42 @@
 package poo.trabalho.view;
 
+import java.util.Date;
+import java.util.Scanner;
+import poo.trabalho.modal.Paciente;
+import poo.trabalho.modal.FichaMedica;
+
 public class PacienteView {
+	
+	public static void printCadastroPaciente() {
+		Scanner scanner = new Scanner(System.in);
+		
+		Paciente paciente = new Paciente(null, null, (char) 0, null, null, null);
+		
+		String nome;
+		String endereco;
+		Date dataNascimento;
+		char sexo;
+		
+	System.out.println("-------------------------------");
+	System.out.println(" \t\t Cadastro Paciente");
+	System.out.println("-------------------------------");
+	System.out.println("Informe o nome do paciente: ");
+	paciente.setNome(scanner.nextLine());
+	System.out.println("Informe o endereco: ");
+	//paciente.getEndereco(scanner.nextLine());
+	System.out.println("Informe o sexo: ");
+	paciente.setSexo(scanner.nextLine().charAt(0));
+	System.out.println("Informe a data de nascimento: ");
+	//paciente.setDataNascimento(scanner.nextLine());
+	System.out.println("-------------------------------");
+	
+	scanner.close();
+	
+	return new Paciente
+	
+	}
+	
+	
+	
 
 }
