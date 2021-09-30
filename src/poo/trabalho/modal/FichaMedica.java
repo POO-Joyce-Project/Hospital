5871e555faf35f400dc173a6abaf5bf13d1521f7
prazerjao <joaoalves1638@gmail.com>
2021-09-29 21:09:24 -0300

    criacao classes medico e ficha medica

--- src/poo/trabalho/modal/FichaMedica.java
+++ src/poo/trabalho/modal/FichaMedica.java
@@ -1,5 +1,63 @@
 package poo.trabalho.modal;
 
 public class FichaMedica {
-
+	
+	private String nome;
+	private String observacao;
+	private String tipoSanguineo;
+	private String medicacaoRegular;
+	private Float altura;
+	private Float peso;
+	
+	//inicializadores
+	public FichaMedica(String nome, String observacao, String tipoSanguineo, String medicacaoRegular, Float altura,
+			Float peso) {
+		super();
+		this.nome = nome;
+		this.observacao = observacao;
+		this.tipoSanguineo = tipoSanguineo;
+		this.medicacaoRegular = medicacaoRegular;
+		this.altura = altura;
+		this.peso = peso;
+	}
+	
+	//getters e setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getObservacao() {
+		return observacao;
+	}
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	public String getTipoSanguineo() {
+		return tipoSanguineo;
+	}
+	public void setTipoSanguineo(String tipoSanguineo) {
+		this.tipoSanguineo = tipoSanguineo;
+	}
+	public String getMedicacaoRegular() {
+		return medicacaoRegular;
+	}
+	public void setMedicacaoRegular(String medicacaoRegular) {
+		this.medicacaoRegular = medicacaoRegular;
+	}
+	public Float getAltura() {
+		return altura;
+	}
+	public void setAltura(Float altura) {
+		this.altura = altura;
+	}
+	public Float getPeso() {
+		return peso;
+	}
+	public void setPeso(Float peso) {
+		this.peso = peso;
+	}
+	
+	
 }
--- src/poo/trabalho/modal/Medico.java
+++ src/poo/trabalho/modal/Medico.java
@@ -1,5 +1,29 @@
 package poo.trabalho.modal;
 
-public class Medico {
-
+public class Medico extends Pessoa{
+	
+	private String crm;
+	private String especialidade;
+	
+	//Inicializador 
+	public Medico(String crm, String especialidade) {
+		super();
+		this.crm = crm;
+		this.especialidade = especialidade;
+	}
+	
+	//Getters e setters
+	public String getCrm() {
+		return crm;
+	}
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	
 }
